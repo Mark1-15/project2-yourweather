e2c4f1783657058134282267cbdd665a98bf5175
Mark1-15 <markmarsden04@gmail.com>
2025-03-07 11:30:14 +0000

    added weatherIcons in script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,3 +2,6 @@
 const apiKey = "0fa7fd2f71334857a73124304252602";
 const apiUrl = "https://www.weatherapi.com/api-explorer.aspx"
 
+const  baseURL = "api.weatherapi.com/v1"
+
+const weatherIcons = "https://www.weatherapi.com/docs/weather_conditions.json"
