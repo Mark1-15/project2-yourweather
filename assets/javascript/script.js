bd3528807f7a1780f21d4761ede4ca94ae4638bf
Mark1-15 <markmarsden04@gmail.com>
2025-03-07 17:29:33 +0000

    created const variables in script.js, and added script tag in index.html to link with script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,3 +5,17 @@ const apiUrl = "https://www.weatherapi.com/api-explorer.aspx"
 const  baseURL = "api.weatherapi.com/v1"
 
 const weatherIcons = "https://www.weatherapi.com/docs/weather_conditions.json"
+
+/* Linking html elements to js file */
+
+const locationInput = document.getElementById("locationInput");
+const day1Temperature = document.getElementById("day1-temp");
+const locationCard = document.getElementById("location-card");
+const searchButton = document.getElementById("searchButton");
+
+/*  Optional checkboxes */
+const wind = document.getElementById("");
+const fahrenheit = document.getElementById("");
+const kph = document.getElementById("");
+const extra = document.getElementById("");
+
