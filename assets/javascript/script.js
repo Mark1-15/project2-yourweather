23bf149ba97f76c46937346b2bad226638b25b8c
Mark1-15 <markmarsden04@gmail.com>
2025-03-10 13:16:54 +0000

    updated api key and url info and added async checkWeather function in script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,30 @@
 /* API Key and URL */ 
-const apiKey = "0fa7fd2f71334857a73124304252602";
-const apiUrl = "https://api.weatherapi.com/v1/current.json?key=`${apiKey}`&q=`${locationInput}`";
+const apiKey = "423b063fbe53a6369dab2add71d4e472";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=berlin";
+
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+}
+
+checkWeather();
+
+/** 
 
 const  baseURL = "api.weatherapi.com/v1"
 
 const weatherIcons = "https://www.weatherapi.com/docs/weather_conditions.json"
 
-/* Linking html elements to js file */
+/* Linking html elements to js file 
 
 const locationInput = document.getElementById("locationInput").value;
 const day1Temperature = document.getElementById("day1-temp");
 const locationCard = document.getElementById("location-card");
 const searchButton = document.getElementById("searchButton");
 
-/*  Optional checkboxes */
+/*  Optional checkboxes 
 const wind = document.getElementById("");
 const fahrenheit = document.getElementById("");
 const kph = document.getElementById("");
@@ -22,26 +33,6 @@ const extra = document.getElementById("");
 /* Search function 
  function searchWeather() {
     searchButton.addEventListener(search);
- } */
+ } 
 
- searchButton.addEventListener('click', () => {
-    const location = locationInput.value;
-    if (location) {
-        fetchWeather(location);
-    }
-});
-
-function fetchWeather(location) {
-    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            locationElement.textContent = data.name;
-            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
-            descriptionElement.textContent = data.weather[0].description;
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-        });
-}
+    */
