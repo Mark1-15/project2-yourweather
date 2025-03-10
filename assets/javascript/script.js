9ed32f3c8904b52922ad94c2998956816e43b765
Mark1-15 <markmarsden04@gmail.com>
2025-03-10 20:59:03 +0000

    Added humidity, pressure, and windspeed variables to script.js and linked them to card in index.html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
 /* API Key and URL */ 
 const apiKey = "423b063fbe53a6369dab2add71d4e472";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchInput = document.getElementById("searchInput");
 const searchButton = document.getElementById("searchButton");
@@ -15,9 +15,13 @@ async function checkWeather(location) {
         console.log(error);
     }
 
-    const locationName = document.getElementById("card-title1").innerHTML = data.name;
-    const temperature = document.getElementById("card-title2").innerHTML = data.main.temp;
-    const description = document.getElementById("card-title3").innerHTML = data.weather;
+    const locationName = document.getElementById("location").innerHTML = data.name;
+    const temperature = document.getElementById("temperature").innerHTML =  "Temperature: " + data.main.temp + "°C";
+    const description = document.getElementById("description").innerHTML = "Description: " + data.weather[0].description;
+    const main = document.getElementById("main").innerHTML = "Weather: " + data.weather[0].main;
+    const humidity = document.getElementById("humidity").innerHTML = "Humidity: " + data.main.humidity + "%";
+    const pressure = document.getElementById("pressure").innerHTML = "Pressure: " + data.main.pressure + "mb";
+    const windspeeed = document.getElementById("windspeed").innerHTML = "Windspeed: " + data.wind.speed + "mph";
 } 
 
 searchButton.addEventListener("click", ()=>{
